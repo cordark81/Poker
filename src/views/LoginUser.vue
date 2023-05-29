f1b6d2280ac1bb58aad903a6962225865664aa45
Ángel <54036253+Aphans@users.noreply.github.com>
2023-05-29 23:00:24 +0200

    a

--- src/views/LoginUser.vue
+++ src/views/LoginUser.vue
@@ -1,6 +1,6 @@
 <template>
   <WelcomeBanner />
-  <section class="bg-red-500 dark:bg-gray-900">
+  <section class="background-login" >
     <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
       <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
         <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
@@ -126,4 +126,10 @@ const submitForgotPassword = async () => {
 </script>
 
 <style scoped>
+.background-login {
+  background-image: url("../assets/background-login.jpg"); /* Reemplaza con la ruta correcta a tu imagen de fondo */
+  background-size: cover;
+  background-position: center;
+  /* Agrega cualquier otra configuración de estilo que desees para el fondo */
+}
 </style>
