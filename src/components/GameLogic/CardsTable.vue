591baab71c482b2e2d0f2ac56e4a100adc8fad4f
IRCdaw <ivanromero.daw@gmail.com>
2023-05-30 23:58:17 +0200

    Subido por ivan integrada funcionalidad de juego en vue

---
+++ src/components/GameLogic/CardsTable.vue
@@ -0,0 +1,17 @@
+<template>
+	<div>
+		<div class="flex">
+			<div v-for="carta in storeCards.cartas_mesa">
+				<img
+					:src="'../../src/assets/cards/' + carta + '.png'"
+					class="w-16 h-24"
+				/>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script setup>
+import { useCardsStore } from "../../stores/cards";
+const storeCards = useCardsStore();
+</script>
--- src/views/Room.vue
+++ src/views/Room.vue
@@ -1,25 +1,47 @@
 <template>
-  <div class="h-screen bg-green-600">
-    <h1 class="text-center font-extrabold pt-5 text-5xl">Sala {{ room }}</h1>
-    <div class="border border-slate-300 rounded-full h-2/4 w-2/4 flex flex-row items-center justify-center mt-20 mx-auto">
-      <div class="flex justify-center flex-wrap gap-16">
-        <div v-for="(seat, index) in seats" :key="index" class="flex w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/4 ">
-          <div v-if="seat.user" class="">
-            <OccupiedSeat @leaveSeat="standUpSeat(index)" :seat="seat" />
-          </div>
-          <div v-else class="">
-            <Seats v-if="!seat.user" @occupeSeat="sitIn(index)" />
-          </div>
-        </div>
-        <Comprobar_con_lib />
-      </div>
-    </div>
-    <Chat :room="room" />
-  </div>
-  <ModalInSeat v-show="showModal" @closeModal="showModal=false"/>
+	<div class="h-screen bg-green-600">
+		<h1 class="text-center font-extrabold pt-5 text-5xl">Sala {{ room }}</h1>
+		<div
+			class="border border-slate-300 rounded-full h-2/4 w-2/4 flex flex-row items-center justify-center mt-20 mx-auto"
+		>
+			<div class="flex justify-center flex-wrap gap-16">
+				<div
+					v-for="(seat, index) in seats"
+					:key="index"
+					class="flex w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/4"
+				>
+					<div v-if="seat.user" class="">
+						<OccupiedSeat
+							@leaveSeat="standUpSeat(index)"
+							:seat="seat"
+							:index="index"
+							:mostrar="repartidas"
+						/>
+					</div>
+					<div v-else class="">
+						<Seats v-if="!seat.user" @occupeSeat="sitIn(index)" />
+					</div>
+				</div>
+				<button @click="storeCards.gamePhase('flop')">Flop</button>
+				<button @click="storeCards.gamePhase('turn')">Turn</button>
+				<button @click="storeCards.gamePhase('river')">River</button>
+				<button @click="storeCards.check(storeCards.dealtCards)">
+					Comprobar
+				</button>
+				<div>
+					<CardsTable />
+				</div>
+				<p>{{ storeCards.winner }}</p>
+			</div>
+		</div>
+		<Chat :room="room" />
+	</div>
+	<ModalInSeat v-show="showModal" @closeModal="showModal = false" />
+	<button @click="probarRepartir">Repartir</button>
 </template>
 
 <script setup>
+import { useCardsStore } from "../stores/cards";
 import { useUserStore } from "../stores/user";
 import { useSeatsStore } from "../stores/seats";
 import { ref, onMounted } from "vue";
@@ -27,53 +49,69 @@ import { useRouter, onBeforeRouteLeave } from "vue-router";
 import { onValue, refDB } from "../utils/firebase";
 import Chat from "../components/Chat/Chat.vue";
 
-import Seats from "../components/Room/Seats.vue"
+import Seats from "../components/Room/Seats.vue";
 import OccupiedSeat from "../components/Room/OccupiedSeat.vue";
-import ModalInSeat from '../components/Modals/ModalInSeat.vue'
-import Comprobar_con_lib from "../components/GameLogic/Comprobar_con_lib.vue";
+import ModalInSeat from "../components/Modals/ModalInSeat.vue";
+import CardsTable from "../components/GameLogic/CardsTable.vue";
 
 const router = useRouter();
 const storeUser = useUserStore();
 const storeSeat = useSeatsStore();
+const storeCards = useCardsStore();
 const room = ref(router.currentRoute.value.params.roomName);
 const seats = ref([]);
 const selectedSeatIndex = ref(-1);
 const showModal = ref(false);
 
-onMounted(() => {
-  const roomRef = refDB(`rooms/${room.value}`);
-  try {
-    onValue(roomRef, (snapshot) => {
-      const roomData = snapshot.val();
-      if (roomData) {
-        seats.value = roomData.seats;
+const repartidas = ref(false);
 
-      }
-    });
-  } catch (error) {
-    console.error("Error listening for room data:", error);
-  }
+onMounted(() => {
+	const roomRef = refDB(`rooms/${room.value}`);
+	try {
+		onValue(roomRef, (snapshot) => {
+			const roomData = snapshot.val();
+			if (roomData) {
+				seats.value = roomData.seats;
+			}
+		});
+	} catch (error) {
+		console.error("Error listening for room data:", error);
+	}
 });
 
-const sitIn = (seatIndex) => {
+const probarRepartir = () => {
+	storeCards.dealingCards(seats.value);
+	console.log(storeCards.dealtCards[1].hand);
+	repartidas.value = true;
+};
 
-  try {
-    const obj = storeSeat.sitInSeat(seatIndex, selectedSeatIndex.value, seats.value, room.value);
-    if (obj.selected !== -1) {
-      selectedSeatIndex.value = obj.selected;
-      showModal.value = obj.modal;
-    }
-  } catch (error) {
-    console.log(error.message)
-  }
-}
+const sitIn = (seatIndex) => {
+	try {
+		const obj = storeSeat.sitInSeat(
+			seatIndex,
+			selectedSeatIndex.value,
+			seats.value,
+			room.value
+		);
+		if (obj.selected !== -1) {
+			selectedSeatIndex.value = obj.selected;
+			showModal.value = obj.modal;
+		}
+	} catch (error) {
+		console.log(error.message);
+	}
+};
 
 const standUpSeat = (seatIndex) => {
-  try {
-    selectedSeatIndex.value = storeSeat.standUpFromSeat(seatIndex, seats.value, room.value)
-  } catch (error) {
-    console.log(error.message)
-  }
+	try {
+		selectedSeatIndex.value = storeSeat.standUpFromSeat(
+			seatIndex,
+			seats.value,
+			room.value
+		);
+	} catch (error) {
+		console.log(error.message);
+	}
 };
 
 const leaveRoom = () => {
