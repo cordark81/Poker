6b6198a799730642ca9d1737aeea59a631872106
IRCdaw <ivanromero.daw@gmail.com>
2023-06-03 14:50:57 +0200

    Añadido quitar fichas del sb y bb

--- src/stores/pot.js
+++ src/stores/pot.js
@@ -1,11 +1,27 @@
 import { defineStore } from "pinia";
 import { ref } from "vue";
+import { refDB, getDB, set, auth, get, numberSeats } from "../utils/firebase";
 
 export const usePotStore = defineStore("potStore", () => {
-	const pot = ref(0);
-	const chips = ref(400);
+  const initialPot = (seats, room) => {
+    seats.forEach((seat, index) => {
+      if (seat.dealer === "bb") {
+        bet(10, room, index);
+      } else if (seat.dealer === "sb") {
+        bet(5, room, index);
+      }
+    });
+  };
 
-	const bet = (playerBet) => {
+  const bet = async (chips, room, index) => {
+    const chipsRef = refDB(`rooms/${room}/seats/${index}/chipsInGame`);
+    let chipsInGame = await getDB(chipsRef);
+    chipsInGame -= chips;
+    set(chipsRef, chipsInGame);
+    //const potRoomRef = refDB(`rooms/${room}/pot`);
+  };
+
+  /*const bet = (playerBet) => {
 		chips.value -= playerBet;
 		pot.value += playerBet;
 		console.log("Chips:" + chips.value);
@@ -17,10 +33,11 @@ export const usePotStore = defineStore("potStore", () => {
 	const resetChips = () => {
 		chips.value = 400;
 		pot.value = 0;
-	};
+	};*/
 
-	return {
-		bet,
-		resetChips,
-	};
+  return {
+    //bet,
+    //resetChips,
+    initialPot,
+  };
 });
--- src/views/Room.vue
+++ src/views/Room.vue
@@ -1,60 +1,77 @@
 <template>
-	<div class="h-screen bg-green-600 background-table">
-		<div class="w-1/5 text-center flex">
-			<h1
-				class="background-room text-black mt-5 ml-5 p-7 rounded-2xl border-2 border-amber-400 font-extrabold text-4xl text-white my-auto">
-				Sala {{ room }}
-			</h1>
-		</div>
+  <div class="h-screen bg-green-600 background-table">
+    <div class="w-1/5 text-center flex">
+      <h1
+        class="background-room text-black mt-5 ml-5 p-7 rounded-2xl border-2 border-amber-400 font-extrabold text-4xl text-white my-auto"
+      >
+        Sala {{ room }}
+      </h1>
+    </div>
 
-		<div class="flex justify-center items-center flex-wrap h-96">
-			<div v-for="(seat, index) in seats" :key="index"
-				class="h-52 flex justify-center w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/4" :class="styleSitInTable(index)">
-				<div v-if="seat.user" class="">
-					<OccupiedSeat @leaveSeat="standUpSeat(index)" :seat="seat" :index="index" :mostrar="repartidas"
-						:room="room" />
-				</div>
-				<div v-else>
-					<Seats v-if="!seat.user" @occupeSeat="sitIn(index)" :room="room" :index="index" />
-				</div>
-				<div>
-					<CardsTable />
-				</div>
-				<button @click="storeCards.asignChipsInGame(room, index)">
-					pruebas
-				</button>
-				<button @click="storeCards.collectChips(room, index)">pruebas2</button>
-			</div>
-			<Chat class="flex flex-col" :room="room" />
-			<GameConsole />
-		</div>
-		<div class="bg-white w-96 flex justify-center">
-			<button @click="probarRepartir">Repartir</button>
-			<button @click="storeCards.gamePhase('flop')">Flop</button>
-			<button @click="storeCards.gamePhase('turn')">Turn</button>
-			<button @click="storeCards.gamePhase('river')">River</button>
+    <div class="flex justify-center items-center flex-wrap h-96">
+      <div
+        v-for="(seat, index) in seats"
+        :key="index"
+        class="h-52 flex justify-center w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/4"
+        :class="styleSitInTable(index)"
+      >
+        <div v-if="seat.user" class="">
+          <OccupiedSeat
+            @leaveSeat="standUpSeat(index)"
+            :seat="seat"
+            :index="index"
+            :mostrar="repartidas"
+            :room="room"
+          />
+        </div>
+        <div v-else>
+          <Seats
+            v-if="!seat.user"
+            @occupeSeat="sitIn(index)"
+            :room="room"
+            :index="index"
+          />
+        </div>
+        <div>
+          <CardsTable />
+        </div>
+      </div>
+      <Chat class="flex flex-col" :room="room" />
+      <GameConsole />
+    </div>
+    <div class="bg-white w-96 flex justify-center">
+      <button @click="probarRepartir">Repartir</button>
+      <button @click="storeCards.gamePhase('flop')">Flop</button>
+      <button @click="storeCards.gamePhase('turn')">Turn</button>
+      <button @click="storeCards.gamePhase('river')">River</button>
 
-			<button @click="storeCards.deleteDealer(seats, room)">
-				Eliminar sorteo
-			</button>
-		</div>
-		<ModalInSeat v-show="showModal" @closeModal="showModal = false" />
-	</div>
+      <button @click="storeCards.deleteDealer(seats, room)">
+        Eliminar sorteo
+      </button>
+      <div class="bg-red-600 ml-6">
+        <p class="text-white">{{ potRoom }}</p>
+      </div>
+    </div>
+    <ModalInSeat v-show="showModal" @closeModal="showModal = false" />
+  </div>
 </template>
 
 <script setup>
 import { useCardsStore } from "../stores/cards";
 import { useUserStore } from "../stores/user";
 import { useSeatsStore } from "../stores/seats";
-import { useGameStore } from "../stores/game"
+import { useGameStore } from "../stores/game";
+import { usePotStore } from "../stores/pot";
 import { ref, onMounted } from "vue";
 import { useRouter, onBeforeRouteLeave } from "vue-router";
 import {
-	onValue,
-	refDB,
-	numberSeats,
-	updateNumberSeats,
-	onPlayersSit,
+  onValue,
+  refDB,
+  getDB,
+  numberSeats,
+  updateNumberSeats,
+  onPlayersSit,
+  set,
 } from "../utils/firebase";
 import Chat from "../components/Chat/Chat.vue";
 import Seats from "../components/Room/Seats.vue";
@@ -68,129 +85,132 @@ const storeUser = useUserStore();
 const storeSeat = useSeatsStore();
 const storeCards = useCardsStore();
 const storeGame = useGameStore();
+const storePot = usePotStore();
 const room = ref(router.currentRoute.value.params.roomName);
 const seats = ref([]);
 const selectedSeatIndex = ref(-1);
 const showModal = ref(false);
 const repartidas = ref(false);
+const potRoom = ref(0);
 
-onMounted(() => {
-	const roomRef = refDB(`rooms/${room.value}`);
-	try {
-		onValue(roomRef, async (snapshot) => {
-			const roomData = await snapshot.val();
-			if (roomData) {
-				seats.value = roomData.seats;
-				checkIndex(seats.value);
-			}
-		});
-		// posible zona de get
-		let ditchDealerDone = false;
-		onPlayersSit("Rooms", room.value, (roomData) => {
+onMounted(async () => {
+  const roomRef = refDB(`rooms/${room.value}`);
+  try {
+    onValue(roomRef, async (snapshot) => {
+      const roomData = await snapshot.val();
+      if (roomData) {
+        seats.value = roomData.seats;
+        potRoom.value = roomData.pot;
+        checkIndex(seats.value);
+      }
+    });
 
-			if (roomData.data().seat === 0) {
-				if (ditchDealerDone === false) {
-					storeGame.ditchDealer(seats.value, room.value);
-					ditchDealerDone = true;
-				}
-				if (seats.value[selectedSeatIndex.value].dealer === "dealer") {
-					storeCards.dealingCards(seats.value, room.value);
-					repartidas.value = true;
-				}
-			} else {
-				console.log("faltan jugadores");
-				storeGame.deleteDealer(seats.value, room.value);
-				storeCards.resetCards(seats.value, room.value);
-				ditchDealerDone = false
-			}
-
-		});
-	} catch (error) {
-		console.log(error.message);
-	}
+    const roomDealerRef = refDB(`rooms/${room.value}/ditchDealerDone`);
+    const ditchDealerDone = await getDB(roomDealerRef);
+    onPlayersSit("Rooms", room.value, (roomData) => {
+      if (roomData.data().seat === 0) {
+        if (ditchDealerDone === false) {
+          storeGame.ditchDealer(seats.value, room.value);
+          storePot.initialPot(seats.value, room.value);
+          set(roomDealerRef, true);
+        }
+        if (seats.value[selectedSeatIndex.value].dealer === "dealer") {
+          storeCards.dealingCards(seats.value, room.value);
+          repartidas.value = true;
+        }
+      } else {
+        console.log("faltan jugadores");
+        storeGame.deleteDealer(seats.value, room.value);
+        storeCards.resetCards(seats.value, room.value);
+        set(roomDealerRef, false);
+      }
+    });
+  } catch (error) {
+    console.log(error.message);
+  }
 });
 
 const styleSitInTable = (index) => {
-	if (index == 1) {
-		return "items-end pb-40";
-	} else if (index == 2) {
-		return "items-end pl-10";
-	} else {
-		return "items-end pr-10";
-	}
+  if (index == 1) {
+    return "items-end pb-40";
+  } else if (index == 2) {
+    return "items-end pl-10";
+  } else {
+    return "items-end pr-10";
+  }
 };
 
 const checkIndex = (seats) => {
-	seats.forEach((seat, index) => {
-		if (seat.user === storeUser.user.displayName) {
-			selectedSeatIndex.value = index;
-		}
-	});
+  seats.forEach((seat, index) => {
+    if (seat.user === storeUser.user.displayName) {
+      selectedSeatIndex.value = index;
+    }
+  });
 };
 
 const sitIn = async (seatIndex) => {
-	try {
-		const obj = storeSeat.sitInSeat(
-			seatIndex,
-			selectedSeatIndex.value,
-			seats.value,
-			room.value
-		);
-		if (obj.selected !== -1) {
-			selectedSeatIndex.value = obj.selected;
-			showModal.value = obj.modal;
-
-			const number = await numberSeats("Rooms", room.value);
-			let seat = number.data().seat - 1;
-			updateNumberSeats("Rooms", room.value, { seat: seat });
-		}
-	} catch (error) {
-		console.log(error.message);
-	}
+  try {
+    const obj = storeSeat.sitInSeat(
+      seatIndex,
+      selectedSeatIndex.value,
+      seats.value,
+      room.value
+    );
+    if (obj.selected !== -1) {
+      selectedSeatIndex.value = obj.selected;
+      showModal.value = obj.modal;
+      await storeGame.asignChipsInGame(room.value, seatIndex);
+      const number = await numberSeats("Rooms", room.value);
+      let seat = number.data().seat - 1;
+      updateNumberSeats("Rooms", room.value, { seat: seat });
+    }
+  } catch (error) {
+    console.log(error.message);
+  }
 };
 
 const standUpSeat = async (seatIndex) => {
-	try {
-		selectedSeatIndex.value = storeSeat.standUpFromSeat(
-			seatIndex,
-			seats.value,
-			room.value
-		);
-		const number = await numberSeats("Rooms", room.value);
-		let seat = number.data().seat + 1;
-		updateNumberSeats("Rooms", room.value, { seat: seat });
-	} catch (error) {
-		console.log(error.message);
-	}
+  try {
+    selectedSeatIndex.value = storeSeat.standUpFromSeat(
+      seatIndex,
+      seats.value,
+      room.value
+    );
+    const number = await numberSeats("Rooms", room.value);
+    let seat = number.data().seat + 1;
+    updateNumberSeats("Rooms", room.value, { seat: seat });
+  } catch (error) {
+    console.log(error.message);
+  }
 };
 
 const leaveRoom = () => {
-	const seatIndex = findSeatIndexByUser(storeUser.user.displayName);
-	if (seatIndex !== -1) {
-		standUpSeat(seatIndex);
-	}
+  const seatIndex = findSeatIndexByUser(storeUser.user.displayName);
+  if (seatIndex !== -1) {
+    standUpSeat(seatIndex);
+  }
 };
 
 const findSeatIndexByUser = (username) => {
-	return seats.value.findIndex((seat) => seat.user === username);
+  return seats.value.findIndex((seat) => seat.user === username);
 };
 
 onBeforeRouteLeave((to, from, next) => {
-	leaveRoom();
-	next();
+  leaveRoom();
+  next();
 });
 </script>
 
 <style scoped>
 .background-table {
-	background-image: url("../assets/images/poker-table-green-cloth-on-dark-background-illustration-free-vector.jpg");
-	background-size: cover;
-	background-position: center;
+  background-image: url("../assets/images/poker-table-green-cloth-on-dark-background-illustration-free-vector.jpg");
+  background-size: cover;
+  background-position: center;
 }
 
 .background-room {
-	background-image: url("../assets/images/31_copia.webp");
-	background-size: cover;
-	background-position: center;
+  background-image: url("../assets/images/31_copia.webp");
+  background-size: cover;
+  background-position: center;
 }
 </style>
