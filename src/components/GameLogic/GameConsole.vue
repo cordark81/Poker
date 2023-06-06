c00529f665e3192f1e61e7bc313dc7fffb6f9c60
Eduardo Muñoz Cano <munoz.cano.eduardo@gmail.com>
2023-06-06 21:58:32 +0200

    prueba logica consola de mandos ajustes en raise-2

--- src/components/GameLogic/GameConsole.vue
+++ src/components/GameLogic/GameConsole.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="absolute bottom-0 right-0 w-64 h-64">
     <input v-model="bet" type="number" step="5" :min=betMin :max=betMax>
-    <button v-if="storeGame.verifySimilarPots(seats)===true" class="bg-orange-500" @click="storeConsole.checkConsole(seats,room)">Check</button>
-    <button v-if="storeGame.verifySimilarPots(seats)===false" class="bg-green-500" @click="logicCall()">Call</button>
-    <button class="bg-fuchsia-500" @click="storeConsole.betConsole(seats,room,index,bet)">Bet</button>
-    <button v-if="storePot.potMax(seats,true)!==0" class="bg-blue-500" @click="storeConsole.raiseConsole(seats,room,index)">Raise</button>
-    <button v-if="storeGame.verifySimilarPots(seats)===false" class="bg-red-500" @click="storeConsole.foldConsole(seats,room,index)">Fold</button>
-    <button class="bg-yellow-500" @click="storeConsole.allInConsole(seats,room,index)">All-In</button>
+    <button v-if="storeGame.verifySimilarPots(seats) === true" class="bg-orange-500"
+      @click="storeConsole.checkConsole(seats, room)">Check</button>
+    <button v-if="storeGame.verifySimilarPots(seats) === false" class="bg-green-500" @click="logicCall()">Call</button>
+    <button class="bg-fuchsia-500" @click="storeConsole.betConsole(seats, room, index, bet)">Bet</button>
+    <button v-if="storePot.potMax(seats, true) !== 0" class="bg-blue-500"
+      @click="storeConsole.raiseConsole(seats, room, index)">Raise</button>
+    <button v-if="storeGame.verifySimilarPots(seats) === false" class="bg-red-500"
+      @click="storeConsole.foldConsole(seats, room, index)">Fold</button>
+    <button class="bg-yellow-500" @click="storeConsole.allInConsole(seats, room, index)">All-In</button>
   </div>
 </template>
 
 <script setup>
 
-import { ref,onMounted } from "vue";
+import { ref, onMounted } from "vue";
 import { useConsoleStore } from "../../stores/console";
 import { useGameStore } from "../../stores/game";
 import { usePotStore } from "../../stores/pot";
@@ -25,11 +28,15 @@ const bet = ref(5);
 const betMin = ref(0);
 const betMax = ref(0);
 
-onMounted(()=>{
-  betMin.value=storePot.potMax(props.seats,true);
-  betMax.value=props.seats[props.index].chipsInGame;
-  console.log(betMax.value)
-  bet.value=betMin.value;
+onMounted(() => {
+  betMin.value = storePot.potMax(props.seats, true);
+  betMax.value = props.seats[props.index].chipsInGame;
+
+  if (betMax.value >= props.seats[props.index].chipsInGame) {
+    bet.value = props.seats[props.index].chipsInGame;
+  } else {
+    bet.value = betMin.value;
+  }
 })
 
 const props = defineProps({
