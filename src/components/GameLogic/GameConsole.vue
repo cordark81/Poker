8fe76b008cb3f2839eadf420f7407845ef428fe5
Eduardo Muñoz Cano <munoz.cano.eduardo@gmail.com>
2023-06-06 19:16:07 +0200

    prueba logica consola de mandos

--- src/components/GameLogic/GameConsole.vue
+++ src/components/GameLogic/GameConsole.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="absolute bottom-0 right-0 w-64 h-64">
     <input v-model="bet" type="number">
-    <button class="bg-orange-500" @click="storeConsole.checkConsole(seats,room)">Check</button>
-    <button class="bg-green-500" @click="logicCall()">Call</button>
+    <button v-if="storeGame.verifySimilarPots" class="bg-orange-500" @click="storeConsole.checkConsole(seats,room)">Check</button>
+    <button v-if="!storeGame.verifySimilarPots" class="bg-green-500" @click="logicCall()">Call</button>
     <button class="bg-fuchsia-500" @click="storeConsole.betConsole(seats,room,index,bet)">Bet</button>
     <button class="bg-blue-500" @click="storeConsole.raiseConsole(seats,room,index)">Raise</button>
-    <button class="bg-red-500" @click="storeConsole.foldConsole(seats,room,index)">Fold</button>
+    <button v-if="storeGame.verifySimilarPots" class="bg-red-500" @click="storeConsole.foldConsole(seats,room,index)">Fold</button>
     <button class="bg-yellow-500" @click="storeConsole.allInConsole(seats,room,index)">All-In</button>
   </div>
 </template>
@@ -14,8 +14,10 @@
 
 import { ref } from "vue";
 import { useConsoleStore } from "../../stores/console";
+import { useGameStore } from "../../stores/game";
 
 const storeConsole = useConsoleStore();
+const storeGame = useGameStore();
 
 const bet = ref(0);
 
