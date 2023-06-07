f3c07afcafeeac784446ebfab048a0946848d2e8
IRCdaw <ivanromero.daw@gmail.com>
2023-06-08 01:11:00 +0200

    Arreglo pasado a version anterior y añadida funcionalidad allin

--- src/components/Room/OccupiedSeat.vue
+++ src/components/Room/OccupiedSeat.vue
@@ -1,76 +1,155 @@
 <template>
-  <div>
-    <CardsHand :index="index" :hand="seat.hand" :user="seat.user" :handCards="handCards" class="flex justify-center" />
-    <div class="w-52 h-28 Seat rounded-full border-2 border-amber-400 flex flex-row justify-between">
-      <div class="flex w-10">
-        <div class="w-52">
-          <button v-if="seat.user === storeUser.user.displayName" type="button" @click="leaveSeat()"
-            class="ml-1 mt-1 px-2 bg-red-600 hover:bg-red-700 text-white rounded-md transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-red-500 hover:scale-105 transform">
-            Dejar
-          </button>
-        </div>
-      </div>
-      <div class="flex flex-col w-40 ml-6 mt-3">
-        <div class="flex flex-row justify-around w-36 mr-2">
-          <img v-if="seat.photoUser" class="w-10 h-10 rounded-full" :src="seat.photoUser" alt="User Photo" />
-          <img v-else class="w-10 h-10 rounded-full mt-3"
-            src="../../assets/images/poker-king-beard-logo-design-260nw-2168601229.webp" alt="User Photo" />
-          <h1 class="text-white mt-5">{{ seat.user }}</h1>
-        </div>
-        <div class="flex justify-center w-36 mb-5">
-          <h1 class="text-red-600 font-bold">{{ seat.chipsInGame }}</h1>
-        </div>
+	<div>
+		<CardsHand
+			:index="index"
+			:hand="seat.hand"
+			:user="seat.user"
+			:handCards="handCards"
+			class="flex justify-center"
+		/>
+		<div
+			class="w-52 h-28 Seat rounded-full border-2 border-amber-400 flex flex-row justify-between"
+		>
+			<div class="flex w-10">
+				<div class="w-52">
+					<button
+						v-if="seat.user === storeUser.user.displayName"
+						type="button"
+						@click="leaveSeat()"
+						class="ml-1 mt-1 px-2 bg-red-600 hover:bg-red-700 text-white rounded-md transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-red-500 hover:scale-105 transform"
+					>
+						Dejar
+					</button>
+				</div>
+			</div>
+			<div class="flex flex-col w-40 ml-6 mt-3">
+				<div class="flex flex-row justify-around w-36 mr-2">
+					<img
+						v-if="seat.photoUser"
+						class="w-10 h-10 rounded-full"
+						:src="seat.photoUser"
+						alt="User Photo"
+					/>
+					<img
+						v-else
+						class="w-10 h-10 rounded-full mt-3"
+						src="../../assets/images/poker-king-beard-logo-design-260nw-2168601229.webp"
+						alt="User Photo"
+					/>
+					<h1 class="text-white mt-5">{{ seat.user }}</h1>
+				</div>
+				<div class="flex justify-center w-36 mb-5">
+					<h1 class="text-red-600 font-bold">{{ seat.chipsInGame }}</h1>
+				</div>
 
-        <div v-if="index===2" class="flex flex-row justify-start w-52 mt-5 -ml-20 overflow-visible">
-          <div v-if="seat.potPlayer !== 0" class="flex flex-col justify-center  -ml-10 overflow-visible">
-            <img src="../../assets/images/stacksinfondo.jpg" class="w-12 h-12 rounded-xl border-2 border-amber-500"
-              alt="">
-            <p class="text-white font-bold text-center ">{{ seat.potPlayer }}</p>
-          </div>
-          <div v-if="seat.dealer !== '' && seat.dealer !== 'dealer'"
-            class="ml-16 text-white font-bold border-2 border-amber-500 bg-black w-8 h-8 text-center rounded-full "> {{
-              seat.dealer }}
-          </div>
-          <div v-if="seat.dealer !== '' && seat.dealer === 'dealer'"
-            class="text-white font-bold border-2 border-amber-500 w-10 h-10 ml-5 ">
-            <img src="../../assets/images/dealersinfond.jpg" alt="" class="w-full h-full">
-          </div>
-        </div>
+				<div
+					v-if="index === 2"
+					class="flex flex-row justify-start w-52 mt-5 -ml-20 overflow-visible"
+				>
+					<div
+						v-if="seat.potPlayer !== 0"
+						class="flex flex-col justify-center -ml-10 overflow-visible"
+					>
+						<img
+							src="../../assets/images/stacksinfondo.jpg"
+							class="w-12 h-12 rounded-xl border-2 border-amber-500"
+							alt=""
+						/>
+						<p class="text-white font-bold text-center">{{ seat.potPlayer }}</p>
+					</div>
+					<div
+						v-if="seat.dealer !== '' && seat.dealer !== 'dealer'"
+						class="ml-16 text-white font-bold border-2 border-amber-500 bg-black w-8 h-8 text-center rounded-full"
+					>
+						{{ seat.dealer }}
+					</div>
+					<div
+						v-if="seat.dealer !== '' && seat.dealer === 'dealer'"
+						class="text-white font-bold border-2 border-amber-500 w-10 h-10 ml-5"
+					>
+						<img
+							src="../../assets/images/dealersinfond.jpg"
+							alt=""
+							class="w-full h-full"
+						/>
+					</div>
+				</div>
 
-        <div v-if="index===1" class="flex flex-row justify-start w-52 mt-5  overflow-visible">
-          <div v-if="seat.potPlayer !== 0" class="flex flex-col justify-center  mt-10 -mr-8 overflow-visible">
-            <img src="../../assets/images/stacksinfondo.jpg" class="w-12 h-12 rounded-xl border-2 border-amber-500"
-              alt="">
-            <p class="text-white font-bold text-center ">{{ seat.potPlayer }}</p>
-          </div>
-          <div v-if="seat.dealer !== '' && seat.dealer !== 'dealer'"
-            class="ml-16 text-white font-bold border-2 border-amber-500 bg-black w-8 h-8 text-center rounded-full "> {{
-              seat.dealer }}
-          </div>
-          <div v-if="seat.dealer !== '' && seat.dealer === 'dealer'"
-            class="text-white font-bold border-2 border-amber-500 w-10 h-10 ml-5 ">
-            <img src="../../assets/images/dealersinfond.jpg" alt="" class="w-full h-full">
-          </div>
-        </div>
+				<div
+					v-if="index === 1"
+					class="flex flex-row justify-start w-52 mt-5 overflow-visible"
+				>
+					<div
+						v-if="seat.potPlayer !== 0"
+						class="flex flex-col justify-center mt-10 -mr-8 overflow-visible"
+					>
+						<img
+							src="../../assets/images/stacksinfondo.jpg"
+							class="w-12 h-12 rounded-xl border-2 border-amber-500"
+							alt=""
+						/>
+						<p class="text-white font-bold text-center">{{ seat.potPlayer }}</p>
+					</div>
+					<div
+						v-if="seat.dealer !== '' && seat.dealer !== 'dealer'"
+						class="ml-16 text-white font-bold border-2 border-amber-500 bg-black w-8 h-8 text-center rounded-full"
+					>
+						{{ seat.dealer }}
+					</div>
+					<div
+						v-if="seat.dealer !== '' && seat.dealer === 'dealer'"
+						class="text-white font-bold border-2 border-amber-500 w-10 h-10 ml-5"
+					>
+						<img
+							src="../../assets/images/dealersinfond.jpg"
+							alt=""
+							class="w-full h-full"
+						/>
+					</div>
+				</div>
 
-        <div v-if="index===0" class="flex flex-row justify-start w-52 mt-5 ml-24  overflow-visible">
-          <div v-if="seat.potPlayer !== 0" class="flex flex-col justify-center  -mr-40 ml-12 overflow-visible">
-            <img src="../../assets/images/stacksinfondo.jpg" class="w-12 h-12 rounded-xl border-2 border-amber-500"
-              alt="">
-            <p class="text-white font-bold text-center ">{{ seat.potPlayer }}</p>
-          </div>
-          <div v-if="seat.dealer !== '' && seat.dealer !== 'dealer'"
-            class="ml-16 text-white font-bold border-2 border-amber-500 bg-black w-8 h-8 text-center rounded-full "> {{
-              seat.dealer }}
-          </div>
-          <div v-if="seat.dealer !== '' && seat.dealer === 'dealer'"
-            class="text-white font-bold border-2 border-amber-500 w-10 h-10 ml-5 ">
-            <img src="../../assets/images/dealersinfond.jpg" alt="" class="w-full h-full">
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+				<div
+					v-if="index === 0"
+					class="flex flex-row justify-start w-52 mt-5 ml-24 overflow-visible"
+				>
+					<div
+						v-if="seat.potPlayer !== 0"
+						class="flex flex-col justify-center -mr-40 ml-12 overflow-visible"
+					>
+						<img
+							src="../../assets/images/stacksinfondo.jpg"
+							class="w-12 h-12 rounded-xl border-2 border-amber-500"
+							alt=""
+						/>
+						<p class="text-white font-bold text-center">{{ seat.potPlayer }}</p>
+					</div>
+					<div
+						v-if="seat.dealer !== '' && seat.dealer !== 'dealer'"
+						class="ml-16 text-white font-bold border-2 border-amber-500 bg-black w-8 h-8 text-center rounded-full"
+					>
+						{{ seat.dealer }}
+					</div>
+					<div
+						v-if="seat.dealer !== '' && seat.dealer === 'dealer'"
+						class="text-white font-bold border-2 border-amber-500 w-10 h-10 ml-5"
+					>
+						<img
+							src="../../assets/images/dealersinfond.jpg"
+							alt=""
+							class="w-full h-full"
+						/>
+					</div>
+					<div
+						v-if="
+							storeGame.checkFoldAndAllIn(seats, room, index, false) === true
+						"
+					>
+						<h1>ALL IN</h1>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script setup>
@@ -85,15 +164,16 @@ const storeGame = useGameStore();
 const emits = defineEmits(["leaveSeat"]);
 
 const leaveSeat = () => {
-  storeGame.collectChips(props.room, props.index);
-  emits("leaveSeat");
+	storeGame.collectChips(props.room, props.index);
+	emits("leaveSeat");
 };
 
 const props = defineProps({
-  room: String,
-  seat: Object,
-  index: Number,
-  handCards: Array,
+	room: String,
+	seat: Object,
+	index: Number,
+	handCards: Array,
+	seats: Array,
 });
 
 const chips = ref(400);
@@ -101,14 +181,14 @@ const chips = ref(400);
 
 <style scoped>
 .Seat {
-  background-image: url("../../assets/images/fodo_sentado.jpg");
-  background-size: cover;
-  background-position: center;
+	background-image: url("../../assets/images/fodo_sentado.jpg");
+	background-size: cover;
+	background-position: center;
 }
 
 .dealer {
-  background-image: url("../../assets/images/dealersinfond.jpg");
-  background-size: cover;
-  background-position: center;
+	background-image: url("../../assets/images/dealersinfond.jpg");
+	background-size: cover;
+	background-position: center;
 }
 </style>
--- src/stores/console.js
+++ src/stores/console.js
@@ -5,155 +5,157 @@ import { usePotStore } from "./pot";
 import { useCardsStore } from "./cards";
 
 export const useConsoleStore = defineStore("consoleStore", () => {
-  const storeGame = useGameStore();
-  const storePot = usePotStore();
-  const storeCards = useCardsStore();
+	const storeGame = useGameStore();
+	const storePot = usePotStore();
+	const storeCards = useCardsStore();
 
-  const checkConsole = async (seats, room) => {
-    const phaseInGameRef = refDB(`rooms/${room}/phaseGame`);
-    const phaseInGame = await getDB(phaseInGameRef);
-    if (storeGame.verifySimilarPots(seats)) {
-      if (phaseInGame === "preflop") {
-        phaseChangeWithoutBet(seats, room, "flop", phaseInGameRef);
-      } else if (phaseInGame === "flop") {
-        const maxPotLeft = await storeGame.evaluateMaxPotLeft(seats, room);
-        console.log(maxPotLeft);
-        if (maxPotLeft === "*") {
-          phaseChangeWithoutBet(seats, room, "turn", phaseInGameRef);
-        } else {
-          storeGame.moveTurnLeft(seats, room);
-        }
-      } else if (phaseInGame === "turn") {
-        const maxPotLeft = await storeGame.evaluateMaxPotLeft(seats, room);
-        console.log(maxPotLeft);
-        if (maxPotLeft === "*") {
-          phaseChangeWithoutBet(seats, room, "river", phaseInGameRef);
-        } else {
-          storeGame.moveTurnLeft(seats, room);
-        }
-      } else if (phaseInGame === "river") {
-        //si todos check evaluar cartas
-      }
-    } else {
-      storeGame.moveTurnLeft(seats, room);
-    }
-  };
+	const checkConsole = async (seats, room) => {
+		const phaseInGameRef = refDB(`rooms/${room}/phaseGame`);
+		const phaseInGame = await getDB(phaseInGameRef);
+		if (storeGame.verifySimilarPots(seats)) {
+			if (phaseInGame === "preflop") {
+				phaseChangeWithoutBet(seats, room, "flop", phaseInGameRef);
+			} else if (phaseInGame === "flop") {
+				const maxPotLeft = await storeGame.evaluateMaxPotLeft(seats, room);
+				console.log(maxPotLeft);
+				if (maxPotLeft === "*") {
+					phaseChangeWithoutBet(seats, room, "turn", phaseInGameRef);
+				} else {
+					storeGame.moveTurnLeft(seats, room);
+				}
+			} else if (phaseInGame === "turn") {
+				const maxPotLeft = await storeGame.evaluateMaxPotLeft(seats, room);
+				console.log(maxPotLeft);
+				if (maxPotLeft === "*") {
+					phaseChangeWithoutBet(seats, room, "river", phaseInGameRef);
+				} else {
+					storeGame.moveTurnLeft(seats, room);
+				}
+			} else if (phaseInGame === "river") {
+				//si todos check evaluar cartas
+			}
+		} else {
+			storeGame.moveTurnLeft(seats, room);
+		}
+	};
 
-  const phaseChangeWithoutBet = (seats, room, phase, phaseInGameRef) => {
-    storeGame.gamePhase(phase, room);
-    storePot.resetPotPlayer(seats, room);
-    storePot.resetMaxPot(seats, room);
-    storeGame.resetTurn(seats, room);
-    storeGame.firstTurnPlayer(seats, room, "maxPot");
-    set(phaseInGameRef, phase);
-  };
-  /*pendiente de eliminar
+	const phaseChangeWithoutBet = (seats, room, phase, phaseInGameRef) => {
+		storeGame.gamePhase(phase, room);
+		storePot.resetPotPlayer(seats, room);
+		storePot.resetMaxPot(seats, room);
+		storeGame.resetTurn(seats, room);
+		storeGame.firstTurnPlayer(seats, room, "maxPot");
+		set(phaseInGameRef, phase);
+	};
+	/*pendiente de eliminar
   const callConsole = async (seats, room, index) => {
     ajustBet(seats, room, index, 1);    
   };*/
 
-  const foldConsole = async (seats, room, index) => {
-    // actualiza a 0 la apuesta del jugador, elimina las cartas de esta ronda
-    // y le ponemos una marca de que el jugador esta fold
+	const foldConsole = async (seats, room, index) => {
+		// actualiza a 0 la apuesta del jugador, elimina las cartas de esta ronda
+		// y le ponemos una marca de que el jugador esta fold
 
-    const potPlayerCallingRef = refDB(`rooms/${room}/seats/${index}/potPlayer`);
-    const handRef = refDB(`rooms/${room}/seats/${index}/hand`);
-    const foldRef = refDB(`rooms/${room}/seats/${index}/fold`);
-    const seatRef = refDB(`rooms/${room}/seats`);
-    const potRef = refDB(`rooms/${room}/pot`);
+		const potPlayerCallingRef = refDB(`rooms/${room}/seats/${index}/potPlayer`);
+		const handRef = refDB(`rooms/${room}/seats/${index}/hand`);
+		const foldRef = refDB(`rooms/${room}/seats/${index}/fold`);
+		const seatRef = refDB(`rooms/${room}/seats`);
+		const potRef = refDB(`rooms/${room}/pot`);
 
-    await set(potPlayerCallingRef, 0);
-    await set(handRef, []);
-    await set(foldRef, "*");
+		await set(potPlayerCallingRef, 0);
+		await set(handRef, []);
+		await set(foldRef, "*");
 
-    const newSeats = await getDB(seatRef);
+		const newSeats = await getDB(seatRef);
 
-    if ((await checkPlayerWithoutFold(newSeats)) === 1) {
-      const indexWinner = findFoldedPlayerIndex(newSeats);
-      const chipsForWinner = await getDB(potRef);
-      await storeGame.showWinner(newSeats[indexWinner], chipsForWinner,room);
-      storeGame.resetGameWithWinner(newSeats, room, indexWinner);
-    } else {
-      storeGame.moveTurnLeft(seats, room);
-    }
-  };
+		if ((await checkPlayerWithoutFold(newSeats)) === 1) {
+			const indexWinner = findFoldedPlayerIndex(newSeats);
+			const chipsForWinner = await getDB(potRef);
+			await storeGame.showWinner(newSeats[indexWinner], chipsForWinner, room);
+			storeGame.resetGameWithWinner(newSeats, room, indexWinner);
+		} else {
+			storeGame.moveTurnLeft(seats, room);
+		}
+	};
 
-  //Funcion dinamica para distintos grados de apuesta
-  const raiseConsole = async (seats, room, index) => {
-    if (seats[index].chipsInGame <= storePot.potMax(seats, true) * 2) {
-      allInConsole(seats, room, index);
-    } else {
-      ajustBet(seats, room, index, 2);
-      storeGame.moveTurnLeft(seats, room);
-    }
-  };
+	//Funcion dinamica para distintos grados de apuesta
+	const raiseConsole = async (seats, room, index) => {
+		if (seats[index].chipsInGame <= storePot.potMax(seats, true) * 2) {
+			allInConsole(seats, room, index);
+		} else {
+			ajustBet(seats, room, index, 2);
+			storeGame.moveTurnLeft(seats, room);
+		}
+	};
 
-  const checkPlayerWithoutFold = async (seats) =>
-    seats.reduce((count, seat) => (seat.fold === "*" ? count - 1 : count), 3);
+	const checkPlayerWithoutFold = async (seats) =>
+		seats.reduce((count, seat) => (seat.fold === "*" ? count - 1 : count), 3);
 
-  const findFoldedPlayerIndex = (seats) =>
-    seats.reduce(
-      (count, seat, index) =>
-        seat.fold === "" && count === -1 ? index : count,
-      -1
-    );
+	const findFoldedPlayerIndex = (seats) =>
+		seats.reduce(
+			(count, seat, index) =>
+				seat.fold === "" && count === -1 ? index : count,
+			-1
+		);
 
-  const allInConsole = async (seats, room, index) => {
-    const chipsInGameRef = refDB(`rooms/${room}/seats/${index}/chipsInGame`);
-    const potPlayerCallingRef = refDB(`rooms/${room}/seats/${index}/potPlayer`);
-    const potRef = refDB(`rooms/${room}/pot`);
+	const allInConsole = async (seats, room, index) => {
+		const chipsInGameRef = refDB(`rooms/${room}/seats/${index}/chipsInGame`);
+		const potPlayerCallingRef = refDB(`rooms/${room}/seats/${index}/potPlayer`);
+		const allInRef = refDB(`rooms/${room}/seats/${index}/allIn`);
+		const potRef = refDB(`rooms/${room}/pot`);
 
-    const potPlayer = await getDB(potPlayerCallingRef);
-    const chipsInGame = await getDB(chipsInGameRef);
-    const pot = await getDB(potRef);
+		const potPlayer = await getDB(potPlayerCallingRef);
+		const chipsInGame = await getDB(chipsInGameRef);
+		const pot = await getDB(potRef);
 
-    await set(potPlayerCallingRef, chipsInGame + potPlayer);
-    await set(chipsInGameRef, 0);
-    await set(potRef, pot + chipsInGame + potPlayer);
+		await set(potPlayerCallingRef, chipsInGame + potPlayer);
+		await set(chipsInGameRef, 0);
+		await set(potRef, pot + chipsInGame + potPlayer);
+		await set(allInRef, "*");
 
-    storeGame.moveTurnLeft(seats, room);
-    //si todos all in avanzar fases hasta el final y evaluar cartas podemos poner un delay de 5 seg entre cartas para darle emocion set timeout
-  };
+		storeGame.moveTurnLeft(seats, room);
+		//si todos all in avanzar fases hasta el final y evaluar cartas podemos poner un delay de 5 seg entre cartas para darle emocion set timeout
+	};
 
-  const ajustBet = async (seats, room, index, multiplier) => {
-    const maxPot = storePot.potMax(seats, true);
+	const ajustBet = async (seats, room, index, multiplier) => {
+		const maxPot = storePot.potMax(seats, true);
 
-    const potPlayerCallingRef = refDB(`rooms/${room}/seats/${index}/potPlayer`);
-    const chipsInGameRef = refDB(`rooms/${room}/seats/${index}/chipsInGame`);
-    const potRef = refDB(`rooms/${room}/pot`);
+		const potPlayerCallingRef = refDB(`rooms/${room}/seats/${index}/potPlayer`);
+		const chipsInGameRef = refDB(`rooms/${room}/seats/${index}/chipsInGame`);
+		const potRef = refDB(`rooms/${room}/pot`);
 
-    const potPlayer = await getDB(potPlayerCallingRef);
-    const chipsInGame = await getDB(chipsInGameRef);
-    const pot = await getDB(potRef);
+		const potPlayer = await getDB(potPlayerCallingRef);
+		const chipsInGame = await getDB(chipsInGameRef);
+		const pot = await getDB(potRef);
 
-    await set(potPlayerCallingRef, maxPot * multiplier);
-    await set(chipsInGameRef, chipsInGame - (maxPot * multiplier - potPlayer));
-    await set(potRef, pot + (maxPot * multiplier - potPlayer));
-  };
+		await set(potPlayerCallingRef, maxPot * multiplier);
+		await set(chipsInGameRef, chipsInGame - (maxPot * multiplier - potPlayer));
+		await set(potRef, pot + (maxPot * multiplier - potPlayer));
+	};
 
-  const betConsole = async (seats, room, index, bet) => {
-    const potPlayerCallingRef = refDB(`rooms/${room}/seats/${index}/potPlayer`);
-    const chipsInGameRef = refDB(`rooms/${room}/seats/${index}/chipsInGame`);
-    const potRef = refDB(`rooms/${room}/pot`);
+	const betConsole = async (seats, room, index, bet) => {
+		const potPlayerCallingRef = refDB(`rooms/${room}/seats/${index}/potPlayer`);
+		const chipsInGameRef = refDB(`rooms/${room}/seats/${index}/chipsInGame`);
+		const potRef = refDB(`rooms/${room}/pot`);
 
-    const potPlayer = await getDB(potPlayerCallingRef);
-    const chipsInGame = await getDB(chipsInGameRef);
-    const pot = await getDB(potRef);
+		const potPlayer = await getDB(potPlayerCallingRef);
+		const chipsInGame = await getDB(chipsInGameRef);
+		const pot = await getDB(potRef);
 
-    await set(potPlayerCallingRef, potPlayer + bet);
-    await set(chipsInGameRef, chipsInGame - bet);
-    await set(potRef, pot + bet);
+		await set(potPlayerCallingRef, potPlayer + bet);
+		await set(chipsInGameRef, chipsInGame - bet);
+		await set(potRef, pot + bet);
 
-    storeGame.moveTurnLeft(seats, room);
-  };
+		storeGame.moveTurnLeft(seats, room);
+	};
 
-  return {
-    foldConsole,
-    checkConsole,
-    raiseConsole,
-    allInConsole,
-    betConsole,
-    phaseChangeWithoutBet,
-    ajustBet,
-  };
+	return {
+		foldConsole,
+		checkConsole,
+		raiseConsole,
+		allInConsole,
+		betConsole,
+		phaseChangeWithoutBet,
+		ajustBet,
+	};
 });
