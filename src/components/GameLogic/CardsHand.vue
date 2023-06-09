407e1e3a11dad18b1e03813ffcd0bc638dc37b70
IRCdaw <ivanromero.daw@gmail.com>
2023-06-09 20:57:21 +0200

    Commit antes del deploy

--- src/components/GameLogic/CardsHand.vue
+++ src/components/GameLogic/CardsHand.vue
@@ -2,13 +2,17 @@
 	<div class="flex">
 		<div v-for="card in handCards" :key="card">
 			<img
-				v-if="user === storeUser.user.displayName || storeGame.allPlayerAllIn(seats) || storeGame.checkFoldIfAllIn(seats) "
-				:src="`../../src/assets/cards/${card}.png`"
+				v-if="
+					user === storeUser.user.displayName ||
+					storeGame.allPlayerAllIn(seats) ||
+					storeGame.checkFoldIfAllIn(seats)
+				"
+				:src="`https://pokerzone-eb346.web.app/assets/${card}.png`"
 				class="w-12 h-20 mr-2"
 			/>
 			<img
 				v-else
-				:src="'../../src/assets/cards/back-red.png'"
+				:src="`https://pokerzone-eb346.web.app/assets/back-red.png`"
 				class="w-12 h-20 mr-2"
 			/>
 		</div>
@@ -26,7 +30,7 @@ const props = defineProps({
 	index: Number,
 	hand: Array,
 	user: String,
-	handCards:Array,
-	seats:Array
+	handCards: Array,
+	seats: Array,
 });
 </script>
